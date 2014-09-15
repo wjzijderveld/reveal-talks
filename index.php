823e53c8c7c36a6aa6d208994b1762b84e1a50bc
Willem-Jan Zijderveld <willemjan@beeldspraak.com>
2014-09-15 22:38:52 +0200

    Added PFCongres uncon talk

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 
     <?php
 
-        $directories = array('010php', '2014-phpsummercamp');
+        $directories = array('010php', '2013-pfcongres-uncon', '2014-phpsummercamp');
 
         foreach ($directories as $directory) {
             $talks = glob(__DIR__ . '/' . $directory . '/*.html');
